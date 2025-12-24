5da96efe183c47c3220e6e9736464b24ec980e38
Nabeel Haider <nabeeltariq788@gmail.com>
2025-12-24 22:41:16 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-// App.js
-
-import Landing from "./Components/Landing/Landing";
-import { ToastContainer } from "react-toastify";
-
-import "react-toastify/dist/ReactToastify.css";
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "./layout/Footer";
 
 function App() {
   return (
-    <>
-      <Landing />
-      <ToastContainer />
-    </>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
